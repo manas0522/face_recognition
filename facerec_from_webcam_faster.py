5e2cea29047dded7ec2509b8abd329b6e8bf2862
Manas Misra <64816275+manas0522@users.noreply.github.com>
2020-12-02 15:12:38 +0530

    Update facerec_from_webcam_faster.py

--- facerec_from_webcam_faster.py
+++ facerec_from_webcam_faster.py
@@ -15,7 +15,7 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("me.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
@@ -58,7 +58,7 @@ while True:
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = "Manas"
+            name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
             # if True in matches:
